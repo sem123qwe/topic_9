f640c2dd13dee0b7024eca3f4c75cc708745cfba
Semy123qwe <salimovsemen43@gmail.com>
2024-04-28 15:40:50 +0600

    №6

--- tasks/part_2_insightful/task_6.py
+++ tasks/part_2_insightful/task_6.py
@@ -1,37 +1,20 @@
 # TODO: Задачу необходимо решить используя встроенные методы строк
+text = input()
 
-ALPHABET: str = ("АЕЁИОУЫЭЮЯAEIOU"
-                 "аеёиоуыэюяaeiou"
-                 "БВГДЖЗЙКЛМНПРСТФХЦЧШЩЪЬBCD"
-                 "FGHJKLMNPQRSTVWXYZбвгджзйклмн"
-                 "прстфхцчшщъьbcdfghjklmnpqrstvwxyz")
-NUMBERS: str = "0987654321"
+counter_num = 0
+counter_letter = 0
+counter_space = 0
+num = 0
+laught = len(text)
+while num < laught:
+    if text[num].isdigit():
+        counter_letter += 1
+    elif text[num].isalpha():
+        counter_num += 1
+    elif text[num] == " ":
+        counter_space += 1
+    num += 1
 
-user_line: str = input()
-count_alp: int = 0
-count_num: int = 0
-count_space: int = 0
-
-i = 0
-laught = len(user_line)
-is_word = False
-
-while i < laught:
-    if user_line[i] == " ":
-        count_space += 1
-        is_word = False
-    else:
-        not is_word
-        is_word = True
-
-    if user_line[i] in ALPHABET:
-        count_alp += 1
-
-    if user_line[i] in NUMBERS:
-        count_num += 1
-
-    i += 1
-
-print(count_alp)
-print(count_num)
-print(count_space)
+print(counter_num)
+print(counter_letter)
+print(counter_space)
