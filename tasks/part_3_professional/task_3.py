739be0db088c5b3b1c782eadeca4c25f223cce4a
Semy123qwe <salimovsemen43@gmail.com>
2024-05-12 14:49:45 +0600

    * № 3, 4

--- tasks/part_3_professional/task_3.py
+++ tasks/part_3_professional/task_3.py
@@ -1,11 +1,21 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
-user_input = int(input())
-print(user_input == 1)
 
-"""
-хлеб = 60
-скидка = 10
+user_discount: int = int(input())
+cost: list[str] = input().split()
 
-сумма_скидки = хлеб * (скидка / 100)
-новая_цена = хлеб - сумма_скидки = 54 
-"""
+# if user_discount >= 100 and user_discount <= 0: 
+#     print('Размер скидки должен быть больше 0' 
+#           'и не должен превышать 100')
+    
+a = 'Название'
+b = 'Цена'
+c = 'Сумма скидки'
+d = 'Новаяцена'
+print(f'{a:<15} ' + f'{b:<10}' + f'{c:<15}' + f'{d:<10}')
+# TODO 'это наверное можно упростить, как только придумаю, сразу сделаю'
+for i in range(len(cost)):
+    amounth_discaunt: float = int(cost[i]) * (user_discount / 100)
+    resalt: float = int(cost[i]) - amounth_discaunt
+    print('Товар ' + f'{i + 1:<10}' + f'{int(cost[i]):<10.2f}' 
+          + f'{amounth_discaunt:<15.2f}' 
+          + f'{resalt:<10.2f}')
+    
--- tasks/part_3_professional/task_4.py
+++ tasks/part_3_professional/task_4.py
@@ -1,1 +1,30 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+age: list[int] = input('Введите возрасты посетителей' 
+                       'через пробел: ').split()
+
+youngs: int = 0
+teenagers: int = 0
+adults: int = 0
+olds: int = 0
+
+for i in range(len(age)):
+    blap = int(age[i])
+    if blap <= 2: 
+        youngs += 1
+    elif blap > 3 and blap <= 12: 
+        teenagers += 1
+    elif blap > 18: 
+        adults += 1
+    else:
+        olds += 1
+
+a = 'Дети до 2-х лет'
+b = 'Дети от 3-х до 12 лет'
+c = 'Взрослые'
+d = 'Пенсионеры '
+print(f'{a:<20} ' + f'{b:<25}' + f'{c:<20}' + f'{d:<20}')
+
+print(f'{youngs:<21}' + f'{teenagers:<25}' 
+      + f'{adults:<20}' + f'{olds:<20}') 
+
+
+print('Общая стоимость билетов: ' + f'{teenagers * 1055 + adults * 1499 + 2099 * olds:>3f}')
