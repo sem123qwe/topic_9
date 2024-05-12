b4706144fdc4f34f345b046ef1417e288bc909ad
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-05-12 10:26:53 +0100

    Проверка ДЗ | 9. Работа со строками[Профессионал]
    - Правильно: №3,
    - Неправильно: №4,
    - Улучшены: №3, №4,
    - На доработку: №2, №4,

--- tasks/part_3_professional/task_3.py
+++ tasks/part_3_professional/task_3.py
@@ -1,21 +1,26 @@
-
 user_discount: int = int(input())
 cost: list[str] = input().split()
 
-# if user_discount >= 100 and user_discount <= 0: 
-#     print('Размер скидки должен быть больше 0' 
-#           'и не должен превышать 100')
-    
-a = 'Название'
-b = 'Цена'
-c = 'Сумма скидки'
-d = 'Новаяцена'
-print(f'{a:<15} ' + f'{b:<10}' + f'{c:<15}' + f'{d:<10}')
-# TODO 'это наверное можно упростить, как только придумаю, сразу сделаю'
-for i in range(len(cost)):
-    amounth_discaunt: float = int(cost[i]) * (user_discount / 100)
-    resalt: float = int(cost[i]) - amounth_discaunt
-    print('Товар ' + f'{i + 1:<10}' + f'{int(cost[i]):<10.2f}' 
-          + f'{amounth_discaunt:<15.2f}' 
-          + f'{resalt:<10.2f}')
-    
+if user_discount >= 100 or user_discount <= 0:
+    print('Размер скидки должен быть больше '
+          '0 и не должен превышать 100')
+else:
+    columns: tuple[str, ...] = ('Название', 'Цена', 'Сумма скидки', 'Новая цена')
+    template: str = '{:<15} {:<10} {:<15} {:<10}'
+
+    # https://www.youtube.com/watch?v=D6-d5yWOBd0
+    print(template.format(*columns))
+
+    for i in range(len(cost)):
+        price: float = float(cost[i])
+        product: str = f'Товар {i + 1}'
+        amounth_discaunt: float = price * (user_discount / 100)
+        resalt: float = price - amounth_discaunt
+
+        print(f'{product:<15} '
+              f'{price:<10.2f} '
+              f'{amounth_discaunt:<15.2f} '
+              f'{resalt:<10.2f}')
+
+    # Встроенная функция enumerate
+    # https://pythonchik.ru/osnovy/cikl-for-v-python
--- tasks/part_3_professional/task_4.py
+++ tasks/part_3_professional/task_4.py
@@ -1,18 +1,19 @@
-age: list[int] = input('Введите возрасты посетителей' 
-                       'через пробел: ').split()
+ages: list[str] = input(
+    'Введите возрасты посетителей через пробел: '
+).split()
 
 youngs: int = 0
 teenagers: int = 0
 adults: int = 0
 olds: int = 0
 
-for i in range(len(age)):
-    blap = int(age[i])
-    if blap <= 2: 
+for age in ages:
+    age = int(age)
+    if age <= 2:
         youngs += 1
-    elif blap > 3 and blap <= 12: 
+    elif age > 3 and age <= 12:
         teenagers += 1
-    elif blap > 18: 
+    elif age > 18:
         adults += 1
     else:
         olds += 1
@@ -21,10 +22,9 @@ a = 'Дети до 2-х лет'
 b = 'Дети от 3-х до 12 лет'
 c = 'Взрослые'
 d = 'Пенсионеры '
-print(f'{a:<20} ' + f'{b:<25}' + f'{c:<20}' + f'{d:<20}')
 
-print(f'{youngs:<21}' + f'{teenagers:<25}' 
-      + f'{adults:<20}' + f'{olds:<20}') 
+print(f'{a:<20} {b:<25}{c:<20}{d:<20}')
+print(f'{youngs:<21}{teenagers:<25}{adults:<20}{olds:<20}')
 
-
-print('Общая стоимость билетов: ' + f'{teenagers * 1055 + adults * 1499 + 2099 * olds:>3f}')
+print('Общая стоимость билетов: '
+      f'{teenagers * 1055 + adults * 1499 + 2099 * olds:>,d}₽')
