31b88879eeb760bc1f6bd5b9087b3e9e95c6b6ea
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-05-26 10:52:52 +0100

    Проверка ДЗ | 9. Работа со строками[Профессионал]
    - Правильно:
    - Неправильно:
    - Улучшены: №4, №5
    - На доработку: №2

--- tasks/part_3_professional/task_4.py
+++ tasks/part_3_professional/task_4.py
@@ -2,6 +2,10 @@ ages: list[str] = input(
     'Введите возрасты посетителей через пробел: '
 ).split()
 
+PRICE_TEENAGERS: int = 1055
+PRICE_ADULTS: int = 2099
+PRICE_OLDS: int = 1449
+
 youngs: int = 0
 teenagers: int = 0
 adults: int = 0
@@ -9,23 +13,24 @@ olds: int = 0
 
 for age in ages:
     age = int(age)
-    if age <= 2:
+    if 0 < age <= 2:
         youngs += 1
-    elif age > 3 and age <= 12:
+    elif 3 <= age <= 12:
         teenagers += 1
-    elif age > 18:
+    elif 12 < age < 65:
         adults += 1
-    else:
+    elif age >= 65:
         olds += 1
 
-a: tuple[str] = (
-    'Дети до 2-х лет', 'Дети от 3-х до 12 лет', 'Взрослые', 'Пенсионеры'
-    )
+out_table: str = '{:<20} {:<25} {:<20} {:<20}'
+columns: tuple[str, ...] = ('Дети до 2-х лет',
+                            'Дети от 3-х до 12 лет',
+                            'Взрослые',
+                            'Пенсионеры')
+result: float = (teenagers * PRICE_TEENAGERS
+                 + adults * PRICE_ADULTS
+                 + PRICE_OLDS * olds)
 
-b: str = '{:<20} {:<25} {:<20} {:<20}' 
-
-c = f'{youngs}{teenagers}{adults}{olds}'
-
-
-print(b.format(*a))
-print(b.format(*c))
+print(out_table.format(*columns))
+print(out_table.format(youngs, teenagers, adults, olds))
+print(f'\nОбщая стоимость билетов: {result:>,d}₽')
--- tasks/part_3_professional/task_5.py
+++ tasks/part_3_professional/task_5.py
@@ -1,1 +1,25 @@
-print('a' + f'{' ':<20}')
+results: list[str] = []
+max_width: int = 0
+
+while True:
+    fio: str = input().strip()
+
+    if fio.lower() == 'end':
+        break
+
+    surname, name, patronymic = fio.title().split()
+    # surname, name, patronymic = ['Ф', 'И', 'О']
+    surname_length: int = len(surname)
+
+    if surname_length > max_width:
+        max_width = surname_length
+
+    results.append(f'{surname} {name[0]}.{patronymic[0]}.')
+
+print(f'{"№":^3} {"ФИО":^{max_width}}')
+# for i in range(len(results)):
+# print(f'{i + 1:0>2} | {results[i]}')
+for index, elem in enumerate(results, start=1):
+    print(f'{index:0>2} | {elem}')
+
+# https://pythonchik.ru/osnovy/cikl-for-v-python
