21bc4ee66fb532fa713fcc156b8269973a4135ad
Semy123qwe <salimovsemen43@gmail.com>
2024-05-26 15:03:06 +0600

    * №4

--- tasks/part_3_professional/task_4.py
+++ tasks/part_3_professional/task_4.py
@@ -1,30 +1,30 @@
-ages: list[str] = input(
-    'Введите возрасты посетителей через пробел: '
-).split()
+# ages: list[str] = input(
+#     'Введите возрасты посетителей через пробел: '
+# ).split()
 
-youngs: int = 0
-teenagers: int = 0
-adults: int = 0
-olds: int = 0
+# youngs: int = 0
+# teenagers: int = 0
+# adults: int = 0
+# olds: int = 0
 
-for age in ages:
-    age = int(age)
-    if age <= 2:
-        youngs += 1
-    elif age > 3 and age <= 12:
-        teenagers += 1
-    elif age > 18:
-        adults += 1
-    else:
-        olds += 1
+# for age in ages:
+#     age = int(age)
+#     if age <= 2:
+#         youngs += 1
+#     elif age > 3 and age <= 12:
+#         teenagers += 1
+#     elif age > 18:
+#         adults += 1
+#     else:
+#         olds += 1
 
-a = 'Дети до 2-х лет'
-b = 'Дети от 3-х до 12 лет'
-c = 'Взрослые'
-d = 'Пенсионеры '
+a: tuple[str] = (
+    'Дети до 2-х лет', 'Дети от 3-х до 12 лет', 'Взрослые', 'Пенсионеры'
+    )
 
-print(f'{a:<20} {b:<25}{c:<20}{d:<20}')
-print(f'{youngs:<21}{teenagers:<25}{adults:<20}{olds:<20}')
+b: str = '{:<20} {:<25} {:<20} {:<20}' 
 
-print('Общая стоимость билетов: '
-      f'{teenagers * 1055 + adults * 1499 + 2099 * olds:>,d}₽')
+# c = f'{youngs}{teenagers}{adults}{olds}'
+
+
+print(b.format(*a))
