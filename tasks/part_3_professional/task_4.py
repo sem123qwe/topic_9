4d6d91b8291231ceee1ab77039d17594f1c0b574
Semy123qwe <salimovsemen43@gmail.com>
2024-05-26 15:31:51 +0600

    *№4

--- tasks/part_3_professional/task_4.py
+++ tasks/part_3_professional/task_4.py
@@ -1,22 +1,22 @@
-# ages: list[str] = input(
-#     'Введите возрасты посетителей через пробел: '
-# ).split()
+ages: list[str] = input(
+    'Введите возрасты посетителей через пробел: '
+).split()
 
-# youngs: int = 0
-# teenagers: int = 0
-# adults: int = 0
-# olds: int = 0
+youngs: int = 0
+teenagers: int = 0
+adults: int = 0
+olds: int = 0
 
-# for age in ages:
-#     age = int(age)
-#     if age <= 2:
-#         youngs += 1
-#     elif age > 3 and age <= 12:
-#         teenagers += 1
-#     elif age > 18:
-#         adults += 1
-#     else:
-#         olds += 1
+for age in ages:
+    age = int(age)
+    if age <= 2:
+        youngs += 1
+    elif age > 3 and age <= 12:
+        teenagers += 1
+    elif age > 18:
+        adults += 1
+    else:
+        olds += 1
 
 a: tuple[str] = (
     'Дети до 2-х лет', 'Дети от 3-х до 12 лет', 'Взрослые', 'Пенсионеры'
@@ -24,7 +24,8 @@ a: tuple[str] = (
 
 b: str = '{:<20} {:<25} {:<20} {:<20}' 
 
-# c = f'{youngs}{teenagers}{adults}{olds}'
+c = f'{youngs}{teenagers}{adults}{olds}'
 
 
 print(b.format(*a))
+print(b.format(*c))
